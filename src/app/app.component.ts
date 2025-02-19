71f11c7a5b637687649c27c7ff0131b91864cb2e
Mário Murín <murin@cshub.cz>
2025-02-19 12:45:47 +0100

    Add new styles and layout mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AgendaContainer, MenuElement, SentinelUser } from '@sentinel/layout';
+import packageJson from '../../projects/frontend-theme/package.json';
 
 @Component({
     selector: 'crczp-root',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
+    agendaContainers: AgendaContainer[] = [
+        {
+            label: 'Long agenda name with many words',
+            children: [
+                {
+                    label: 'Long menu item name with many words',
+                },
+                {
+                    label: 'Short'
+                }
+            ]
+        },
+        {
+            label: 'Agenda many items',
+            children: [
+                ...[...Array(15).keys()].map(
+                    i => ({
+                        label: `Item ${i+1}`
+                    } as MenuElement)
+                )
+            ]
+        }
+    ];
 
+    user: SentinelUser = {
+        name: "Example User",
+        login: "user@noreply.com",
+        pictureSrc: "/assets/logo/logo-blue.svg"
+    }
 
+    /**
+     * Pulls version from package.json
+     */
+    getVersion(): string {
+        return packageJson.version
+    }
 }
